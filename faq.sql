5f4b836432565e1c8ba943e3c71ab19725648202
MaxIvashchenko <40541533+MaxIvashchenko@users.noreply.github.com>
2018-09-04 14:43:32 +0300

    Update faq.sql

--- faq.sql
+++ faq.sql
@@ -1,32 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Июл 23 2018 г., 19:24
--- Версия сервера: 5.7.16
--- Версия PHP: 7.0.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- База данных: `diplom`
---
 CREATE DATABASE IF NOT EXISTS `diplom` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `diplom`;
 
 -- --------------------------------------------------------
-
---
--- Структура таблицы `answers`
---
 
 CREATE TABLE `answers` (
   `id` int(10) UNSIGNED NOT NULL,
